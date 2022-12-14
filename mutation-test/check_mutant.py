90edce566a0d3c9d35bb9954e14b32576b5223d1
pingshiyu <pingshiyu@gmail.com>
2022-12-14 01:12:55 +0000

    adding mutation checking script

---
+++ mutation-test/check_mutant.py
@@ -0,0 +1,10 @@
+"""
+cmd line interface to check if the script has mutation-differential on mutation x.
+"""
+
+import argparse
+
+def load_args() -> argparse.Argument
+
+if __name__ == '__main__':
+    pass
